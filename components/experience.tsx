8d1f99efa18e3ecf6b022ca4bf6f50bd0fdd05c0
ArminKar <125621869+ArminKar@users.noreply.github.com>
2023-03-08 21:55:10 +0100

    Resume with next.js and typescript

---
+++ components/experience.tsx
@@ -0,0 +1,47 @@
+import { IWork } from "../data/data";
+
+interface ExperienceProps {
+    data: IWork[];
+}
+
+const Experience = ({ data }: ExperienceProps) => {
+    return (
+        <>
+            <h3>Experience</h3>
+            <article className="Experience">
+                {data.map((work, index) => (
+                        <div key={`work_${index}`} className="Experience-work">
+                            {!!work.description.length && ( 
+                            <ul className="Experience-work-description">
+                                {work.description.map((point, index) => (
+                                    <li key={`point_${index}`} >{point}</li>
+                                ))}
+                                </ul>
+                                )}
+                            </div>
+                    ))}
+            </article>
+            <style jsx>
+                {`
+                    .Experience {
+                        display: flex;
+                        flex-direction: column;    
+                        padding: 0 1rem;
+                    }
+                    .Experience-work {
+                        display: flex;
+                        flex-direction: column;
+                        padding: 3rem 0;
+                        border-bottom: 1px solid rgba(200,200,200, .2);
+                    }
+                    .Experience-work:first-child{
+                        padding-top: 0;
+
+                    }
+                `}
+            </style>
+        </>
+    );
+}
+
+export default Experience;
---
+++ components/social.tsx
@@ -0,0 +1,50 @@
+import { ISocial } from "../data/data";
+
+interface SocialProps {
+    data: ISocial[];
+}
+
+const Social = ({ data }: SocialProps) => {
+    return (
+        <>
+            <h3>My social accounts</h3>
+            <article className="Social">
+                {
+                    data.map((social, index) => (
+                        <div className="Social-type" key={`skill_${index}`} >
+                            <img src={social.icon} />
+                            <a href={social.link} target="_blank">{social.name}</a>
+                        </div>
+                    ))
+                }
+            </article>
+            <style jsx>
+                {`
+                    .Social {
+                        display: flex;
+                        flex-direction: column;    
+                        padding: 0 1rem;
+                        padding-bottom: 3rem;
+                    }
+                    .Social-type{
+                        display: flex;
+                        align-items: center;
+                        margin: .5rem 0;
+                    }
+
+                    .Social-type img{
+                        width: 20px;
+                        margin-right: .5rem;
+                    }
+
+                    .Social-type a{
+                        text-decoration: none;
+                        color: black;
+                    }
+                `}
+            </style>
+        </>
+    );
+}
+
+export default Social;
